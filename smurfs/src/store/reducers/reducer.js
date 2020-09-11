c1165caceed5a8e5724880dcd310bbfa200807bf
ajg7 <ajgebara@gmail.com>
2020-09-11 10:23:53 -0500

    set up initial state for smurfs

--- smurfs/src/store/reducers/reducer.js
+++ smurfs/src/store/reducers/reducer.js
@@ -1,3 +1,21 @@
-export const reducer = () => {
-    
+
+import { 
+    FETCH_SMURFS,
+    FETCH_SMURFS_SUCCESS,
+    FETCH_SMURFS_ERROR,
+    ADD_SMURFS,
+    ADD_SMURFS_SUCCESS,
+    ADD_SMURFS_ERROR
+} from "../actions"
+
+const initialState = {
+    name: "",
+    age: null,
+    height: "",
+    id: null
+}
+
+
+export const reducer = (state = initialState, action) => {
+
 }
