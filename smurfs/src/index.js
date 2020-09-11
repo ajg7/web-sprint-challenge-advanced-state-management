25f048350f24c87b8701db9532248e95d4d50295
ajg7 <ajgebara@gmail.com>
2020-09-11 10:17:02 -0500

    set up project

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,4 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
+//redux set up
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import { reducer } from "./store/reducers/reducer";
+
+const store = createStore(reducer, applyMiddleware(thunk, logger))
+
 ReactDOM.render(<App />, document.getElementById("root"));
